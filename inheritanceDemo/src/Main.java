b68d5bdc68b40083e22e0feded7e575f0137f64a
elifnursirin <elifsirinn@outlook.com>
2022-09-25 11:21:07 +0300

    dosya eklendi

---
+++ inheritanceDemo/src/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        TarimKrediManager tarimKrediManager = new TarimKrediManager();
+        OgretmenKrediManager ogretmenKrediManager = new OgretmenKrediManager();
+        KrediUI krediUI = new KrediUI();
+
+        //polimorfizm
+        krediUI.KrediHesapla(ogretmenKrediManager);
+
+    }
+}
